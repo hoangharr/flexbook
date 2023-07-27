ac19775c1368c7a266ad8cfd0b820058df5b8422
hoangharr <gigabyte.rp@gmail.com>
2023-07-27 16:47:52 +0700

    fixed minor2

--- src/main/java/com/example/flexbook/models/User.java
+++ src/main/java/com/example/flexbook/models/User.java
@@ -5,7 +5,6 @@ import lombok.*;
 
 import java.util.*;
 
-
 @Entity
 @Table(name = "users")
 @Data
@@ -15,12 +14,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int user_id;
+    @Column(nullable = false, length = 50, unique = true)
     private String username;
+    @Column(nullable = false, length = 100, unique = true)
     private String email;
+    @Column(nullable = false)
     private String password;
     private String profile_picture;
     private String bio;
+    @Column(nullable = false)
     private Date created_at;
+    @Column(nullable = false)
     private Date updated_at;
 
 }
